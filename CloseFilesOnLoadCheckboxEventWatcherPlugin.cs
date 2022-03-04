1c4ce92ababa3526a4b458cb7522dc14732d089e
Juan Rio Pizzella <jpizzella@fosterandpartners.com>
2022-03-04 20:33:45 +0000

    Updated whole code and application for it to correspond to Navisworks Manage 2022.

--- CloseFilesOnLoadCheckboxEventWatcherPlugin.cs
+++ CloseFilesOnLoadCheckboxEventWatcherPlugin.cs
@@ -1,4 +1,4 @@
-﻿//Developed for Navisworks Manage 2021, targetting the 4.7.2 .NET Framework.
+﻿//Developed for Navisworks Manage 2022, targetting the 4.7.2 .NET Framework.
 
 using System.Windows.Forms;
 using Autodesk.Navisworks.Api.Plugins;
@@ -16,7 +16,7 @@ namespace CloseFilesOnLoadCheckbox
                 ApplicationControl.CloseFileAfterLoad = true;
 
                 MessageBox.Show(
-                    "Since the 'Close NWC/NWD files on load' checkbox was identified to be unchecked while opening Manage 2021, it has now been automatically ticked." +
+                    "Since the 'Close NWC/NWD files on load' checkbox was identified to be unchecked while opening Navisworks Manage, it has now been automatically ticked." +
                     "\n" + "\n" +
                     "You can manually access the Options Editor to revert this action, but take into consideration that after doing this you may be blocking the overwriting of appended NWC files." +
                     "\n" + "\n" + "\n" + "\n" + "\n" +
